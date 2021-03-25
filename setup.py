60192962d26022f07ccbfb93e467772efb3df7cd
gitduk <wdkany@gmail.com>
2021-03-25 16:32:27 +0800

    add retry_pipeline, del response_pipeline ...

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/gitduk/espider'
 EMAIL = 'wdkany@gmail.com'
 AUTHOR = 'kaige'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.2'
+VERSION = '0.3.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
